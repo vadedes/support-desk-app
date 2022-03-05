c9acd4b224285e4c3f5b31cb2f4696bdb6c45280
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-05 15:19:10 +0800

    Added basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Server...');
+const express = require('express');
+const router = require('./routes/userRoutes');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//Test route
+app.get('/', (req, res) => {
+  // res.send('Hello Dave!');
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`'Server started on PORT: ${PORT}`);
+});
