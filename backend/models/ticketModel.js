134554c5c2a3191dc0c236a98d99c70eb42e6dab
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-07 21:15:48 +0800

    Added ticket model and ticket routes

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: 'User',
+    },
+    product: {
+      type: String,
+      require: [true, 'Please select a product'],
+      enum: ['iPhone', 'Macbook Pro', 'iMac', 'iPad'],
+    },
+    description: {
+      type: String,
+      require: [true, 'Please a description of the issue'],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['new', 'open', 'closed'],
+      default: 'new',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Ticket', ticketSchema);
