2e5c32a49c672c56b6f04ab77c396526c9264fb4
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-09 00:20:40 +0800

    Resolved issue on ticket data state getting reset when viewed by the ticket owner

--- frontend/src/pages/Tickets.js
+++ frontend/src/pages/Tickets.js
@@ -3,22 +3,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 import Spinner from '../components/Spinner';
 import BackButton from '../components/BackButton';
+import TicketItem from '../components/TicketItem';
 
 function Tickets() {
   const { tickets, isLoading, isSuccess } = useSelector(
-    (state) => state.ticket
+    (state) => state.tickets
   );
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (isSuccess) {
-      dispatch(reset());
-    }
-  }, [dispatch, isSuccess]);
+  // useEffect(() => {
+  //   // if (isSuccess) {
+  //   //   dispatch(reset());
+  //   // }
+  // }, [dispatch, isSuccess]);
 
   useEffect(() => {
     dispatch(getTickets());
+    return () => dispatch(reset());
   }, [dispatch]);
 
   if (isLoading) {
@@ -26,9 +28,21 @@ function Tickets() {
   }
 
   return (
-    <div>
+    <>
+      <BackButton url='/' />
       <h1>Tickets</h1>
-    </div>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   );
 }
 export default Tickets;
